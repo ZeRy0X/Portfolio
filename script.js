05c1e5027548729a0e3f694e997b32dae8d686a4
ZeRy0X <100872182+ZeRy0X@users.noreply.github.com>
2024-01-14 18:35:09 +0100

    KnowledgePage

--- script.js
+++ script.js
@@ -29,3 +29,16 @@ const amBtn = function() {
    }
 
 }
+
+
+
+// const bg = document.querySelector('#btn');
+// const windowWidth = window.innerWidth / 5;
+// const windowHeight = window.innerHeight / 5 ;
+
+// bg.addEventListener('mousemove', (e) => {
+//   const mouseX = e.clientX / windowWidth;
+//   const mouseY = e.clientY / windowHeight;
+  
+//   bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+// });
